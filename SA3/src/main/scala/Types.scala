c4a0eb58223f7df75cdbbfaf9acfae9d3df099d3
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2022-01-06 15:49:08 +0100

    Update Types.scala

--- SA3/src/main/scala/Types.scala
+++ SA3/src/main/scala/Types.scala
@@ -12,10 +12,8 @@ case object Resort extends PropertyType
 case object NON extends PropertyType
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 case class client(name: String, age: Int, passport_number: String)
-case class property(id: Int, name: String, PropertyType: PropertyType, category: Int, country: String, city: String,
-                    var NotAvailable: List[Date], Price: Double)
-case class AvailableProperty(id: Int, name: String, PropertyType: PropertyType, category: Int, country: String,
-                             city: String, price: Double)
+case class property(id: Int, name: String, PropertyType: PropertyType, category: Int, country: String, city: String, var NotAvailable: List[Date], Price: Double)
+case class AvailableProperty(id: Int, name: String, PropertyType: PropertyType, category: Int, country: String, city: String, price: Double)
 case class AvailableProperties(var availableProperties: List[AvailableProperty])
 case class SendProperty(date: Date, propertyType: PropertyType, actorRef: ActorRef)
 case class SendPropertyName(date: Date, name: String, actorRef: ActorRef)
@@ -25,8 +23,6 @@ case class SendPropertyCountryCityName(date: Date, country: String, city: String
 case class SendPropertyCountryCityCategory(date: Date, country: String, city: String, category: Int, actorRef: ActorRef)
 case class SendPropertyNameCategory(date: Date, category: Int, name: String, actorRef: ActorRef)
 case class SendPropertyTypeName(date: Date, propertyType: PropertyType, name: String, actorRef: ActorRef)
-case class SendPropertyTypeCountryCity(date: Date, propertyType: PropertyType, country: String, city: String,
-                                       actorRef: ActorRef)
-case class SendPropertyTypeCountryCityName(date: Date, propertyType: PropertyType, country: String, city: String,
-                                           name: String, actorRef: ActorRef)
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////pe
+case class SendPropertyTypeCountryCity(date: Date, propertyType: PropertyType, country: String, city: String, actorRef: ActorRef)
+case class SendPropertyTypeCountryCityName(date: Date, propertyType: PropertyType, country: String, city: String, name: String, actorRef: ActorRef)
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
