260d24d2c0fad63c8274afb0fcabc5cf7db9aa10
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2022-01-07 09:48:03 +0100

    Update Main.scala

--- SA3/src/main/scala/Main.scala
+++ SA3/src/main/scala/Main.scala
@@ -16,20 +16,20 @@ case object NON extends PropertyType
 case class clients(name: String, age: Int, passport_number: String)
 case class property(id: Int, name: String, PropertyType: PropertyType, category: Int, country: String, city: String,
                     var NotAvailable: List[Date], Price: Double)
-case class AvailableProperty(id: Int, name: String, PropertyType: PropertyType, category: Int, country: String,
+case class AvailableProperty(customerName: String, id: Int, name: String, PropertyType: PropertyType, category: Int, country: String,
                              city: String, price: Double)
 case class AvailableProperties(var availableProperties: List[AvailableProperty])
-case class SendProperty(date: Date, propertyType: PropertyType, actorRef: ActorRef)
-case class SendPropertyName(date: Date, name: String, actorRef: ActorRef)
-case class SendPropertyCategory(date: Date, category: Int, actorRef: ActorRef)
-case class SendPropertyCountryCity(date: Date, country: String, city: String, actorRef: ActorRef)
-case class SendPropertyCountryCityName(date: Date, country: String, city: String, name: String, actorRef: ActorRef)
-case class SendPropertyCountryCityCategory(date: Date, country: String, city: String, category: Int, actorRef: ActorRef)
-case class SendPropertyNameCategory(date: Date, category: Int, name: String, actorRef: ActorRef)
-case class SendPropertyTypeName(date: Date, propertyType: PropertyType, name: String, actorRef: ActorRef)
-case class SendPropertyTypeCountryCity(date: Date, propertyType: PropertyType, country: String, city: String,
+case class SendProperty(customerName: String, date: Date, propertyType: PropertyType, actorRef: ActorRef)
+case class SendPropertyName(customerName: String, date: Date, name: String, actorRef: ActorRef)
+case class SendPropertyCategory(customerName: String, date: Date, category: Int, actorRef: ActorRef)
+case class SendPropertyCountryCity(customerName: String, date: Date, country: String, city: String, actorRef: ActorRef)
+case class SendPropertyCountryCityName(customerName: String, date: Date, country: String, city: String, name: String, actorRef: ActorRef)
+case class SendPropertyCountryCityCategory(customerName: String, date: Date, country: String, city: String, category: Int, actorRef: ActorRef)
+case class SendPropertyNameCategory(customerName: String, date: Date, category: Int, name: String, actorRef: ActorRef)
+case class SendPropertyTypeName(customerName: String, date: Date, propertyType: PropertyType, name: String, actorRef: ActorRef)
+case class SendPropertyTypeCountryCity(customerName: String, date: Date, propertyType: PropertyType, country: String, city: String,
                                        actorRef: ActorRef)
-case class SendPropertyTypeCountryCityName(date: Date, propertyType: PropertyType, country: String, city: String,
+case class SendPropertyTypeCountryCityName(customerName: String, date: Date, propertyType: PropertyType, country: String, city: String,
                                            name: String, actorRef: ActorRef)
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class Client (PASSPORT: String, DATE: Date, PT: PropertyType, NAME: String, CITY: String, COUNTRY: String, CATEGORY: Int,
@@ -46,8 +46,10 @@ class Client (PASSPORT: String, DATE: Date, PT: PropertyType, NAME: String, CITY
     clients("Rachel Green", 56, "Y44986746"),
     clients("Petter Griffin", 68, "Y44986747")
   )
+  var CUSTOMER_NAME = ""
   CLIENTS.foreach { x =>
     if (x.passport_number == PASSPORT) {
+      CUSTOMER_NAME = x.name
       println(s"Welcome to the system ${x.name}.")
       println("#######################################################################################################")
     }
@@ -62,7 +64,7 @@ class Client (PASSPORT: String, DATE: Date, PT: PropertyType, NAME: String, CITY
               Thread.sleep(100)
               println("#######################################################################################################")
               Thread.sleep(1000)
-              searchActor ! SendProperty(DATE, PT, self)
+              searchActor ! SendProperty(CUSTOMER_NAME, DATE, PT, self)
             }
           }
         }
@@ -77,7 +79,7 @@ class Client (PASSPORT: String, DATE: Date, PT: PropertyType, NAME: String, CITY
               Thread.sleep(100)
               println("#######################################################################################################")
               Thread.sleep(1000)
-              searchActor ! SendPropertyName(DATE, NAME, self)
+              searchActor ! SendPropertyName(CUSTOMER_NAME, DATE, NAME, self)
             }
           }
         }
@@ -92,7 +94,7 @@ class Client (PASSPORT: String, DATE: Date, PT: PropertyType, NAME: String, CITY
               Thread.sleep(100)
               println("#######################################################################################################")
               Thread.sleep(1000)
-              searchActor ! SendPropertyCategory(DATE, CATEGORY, self)
+              searchActor ! SendPropertyCategory(CUSTOMER_NAME, DATE, CATEGORY, self)
             }
           }
         }
@@ -107,7 +109,7 @@ class Client (PASSPORT: String, DATE: Date, PT: PropertyType, NAME: String, CITY
               Thread.sleep(100)
               println("#######################################################################################################")
               Thread.sleep(1000)
-              searchActor ! SendPropertyCountryCity(DATE, COUNTRY, CITY, self)
+              searchActor ! SendPropertyCountryCity(CUSTOMER_NAME, DATE, COUNTRY, CITY, self)
             }
           }
         }
@@ -122,7 +124,7 @@ class Client (PASSPORT: String, DATE: Date, PT: PropertyType, NAME: String, CITY
               Thread.sleep(100)
               println("#######################################################################################################")
               Thread.sleep(1000)
-              searchActor ! SendPropertyCountryCityName(DATE, COUNTRY, CITY, NAME, self)
+              searchActor ! SendPropertyCountryCityName(CUSTOMER_NAME, DATE, COUNTRY, CITY, NAME, self)
             }
           }
         }
@@ -137,7 +139,7 @@ class Client (PASSPORT: String, DATE: Date, PT: PropertyType, NAME: String, CITY
               Thread.sleep(100)
               println("#######################################################################################################")
               Thread.sleep(1000)
-              searchActor ! SendPropertyCountryCityCategory(DATE, COUNTRY, CITY, CATEGORY, self)
+              searchActor ! SendPropertyCountryCityCategory(CUSTOMER_NAME, DATE, COUNTRY, CITY, CATEGORY, self)
             }
           }
         }
@@ -152,7 +154,7 @@ class Client (PASSPORT: String, DATE: Date, PT: PropertyType, NAME: String, CITY
               Thread.sleep(100)
               println("#######################################################################################################")
               Thread.sleep(1000)
-              searchActor ! SendPropertyNameCategory(DATE, CATEGORY, NAME, self)
+              searchActor ! SendPropertyNameCategory(CUSTOMER_NAME, DATE, CATEGORY, NAME, self)
             }
           }
         }
@@ -167,7 +169,7 @@ class Client (PASSPORT: String, DATE: Date, PT: PropertyType, NAME: String, CITY
               Thread.sleep(100)
               println("#######################################################################################################")
               Thread.sleep(1000)
-              searchActor ! SendPropertyTypeName(DATE, PT, NAME, self)
+              searchActor ! SendPropertyTypeName(CUSTOMER_NAME, DATE, PT, NAME, self)
             }
           }
         }
@@ -182,7 +184,7 @@ class Client (PASSPORT: String, DATE: Date, PT: PropertyType, NAME: String, CITY
               Thread.sleep(100)
               println("#######################################################################################################")
               Thread.sleep(1000)
-              searchActor ! SendPropertyTypeCountryCity(DATE, PT, COUNTRY, CITY, self)
+              searchActor ! SendPropertyTypeCountryCity(CUSTOMER_NAME, DATE, PT, COUNTRY, CITY, self)
             }
           }
         }
@@ -197,7 +199,7 @@ class Client (PASSPORT: String, DATE: Date, PT: PropertyType, NAME: String, CITY
               Thread.sleep(100)
               println("#######################################################################################################")
               Thread.sleep(1000)
-              searchActor ! SendPropertyTypeCountryCityName(DATE, PT, COUNTRY, CITY, NAME, self)
+              searchActor ! SendPropertyTypeCountryCityName(CUSTOMER_NAME, DATE, PT, COUNTRY, CITY, NAME, self)
             }
           }
         }
@@ -247,65 +249,65 @@ class SystemService extends Actor{
     property(25, "Eram", Apartment, 4, "Iran", "Shiraz", List(), 24)
   )
   override def receive: Receive = {
-    case SendProperty(date, propertyType, replyTo) => {
+    case SendProperty(customerName, date, propertyType, replyTo) => {
       var APS = AvailableProperties(List())
       PROPERTIES.foreach {  X =>
         if (!X.NotAvailable.contains(date)){
           if (X.PropertyType == propertyType){
-            APS.availableProperties = APS.availableProperties :+ AvailableProperty(X.id, X.name, X.PropertyType,
-              X.category, X.country, X.city, X.Price)
+            APS.availableProperties = APS.availableProperties :+ AvailableProperty(customerName, X.id, X.name,
+              X.PropertyType, X.category, X.country, X.city, X.Price)
           }
         }
       }
       replyTo ! APS
     }
-    case SendPropertyName(date, name, replyTo) => {
+    case SendPropertyName(customerName, date, name, replyTo) => {
       var APS = AvailableProperties(List())
       PROPERTIES.foreach {  X =>
         if (!X.NotAvailable.contains(date)){
           if (X.name == name){
-            APS.availableProperties = APS.availableProperties :+ AvailableProperty(X.id, X.name, X.PropertyType,
-              X.category, X.country, X.city, X.Price)
+            APS.availableProperties = APS.availableProperties :+ AvailableProperty(customerName, X.id, X.name,
+              X.PropertyType, X.category, X.country, X.city, X.Price)
           }
         }
       }
       replyTo ! APS
     }
-    case SendPropertyCategory(date, category, replyTo) => {
+    case SendPropertyCategory(customerName, date, category, replyTo) => {
       var APS = AvailableProperties(List())
       PROPERTIES.foreach {  X =>
         if (!X.NotAvailable.contains(date)){
           if (X.category == category){
-            APS.availableProperties = APS.availableProperties :+ AvailableProperty(X.id, X.name, X.PropertyType,
-              X.category, X.country, X.city, X.Price)
+            APS.availableProperties = APS.availableProperties :+ AvailableProperty(customerName, X.id, X.name,
+              X.PropertyType, X.category, X.country, X.city, X.Price)
           }
         }
       }
       replyTo ! APS
     }
-    case SendPropertyCountryCity(date, country, city, replyTo) => {
+    case SendPropertyCountryCity(customerName, date, country, city, replyTo) => {
       var APS = AvailableProperties(List())
       PROPERTIES.foreach {  X =>
         if (!X.NotAvailable.contains(date)){
           if (X.country == country){
             if (X.city == city){
-              APS.availableProperties = APS.availableProperties :+ AvailableProperty(X.id, X.name, X.PropertyType,
-                X.category, X.country, X.city, X.Price)
+              APS.availableProperties = APS.availableProperties :+ AvailableProperty(customerName, X.id, X.name,
+                X.PropertyType, X.category, X.country, X.city, X.Price)
             }
           }
         }
       }
       replyTo ! APS
     }
-    case SendPropertyCountryCityName(date, country, city, name, replyTo) => {
+    case SendPropertyCountryCityName(customerName, date, country, city, name, replyTo) => {
       var APS = AvailableProperties(List())
       PROPERTIES.foreach {  X =>
         if (!X.NotAvailable.contains(date)){
           if (X.country == country){
             if (X.city == city){
               if (X.name == name){
-                APS.availableProperties = APS.availableProperties :+ AvailableProperty(X.id, X.name, X.PropertyType,
-                  X.category, X.country, X.city, X.Price)
+                APS.availableProperties = APS.availableProperties :+ AvailableProperty(customerName, X.id, X.name,
+                  X.PropertyType, X.category, X.country, X.city, X.Price)
               }
             }
           }
@@ -313,15 +315,15 @@ class SystemService extends Actor{
       }
       replyTo ! APS
     }
-    case SendPropertyCountryCityCategory(date, country, city, category, replyTo) => {
+    case SendPropertyCountryCityCategory(customerName, date, country, city, category, replyTo) => {
       var APS = AvailableProperties(List())
       PROPERTIES.foreach {  X =>
         if (!X.NotAvailable.contains(date)){
           if (X.country == country){
             if (X.city == city){
               if (X.category == category){
-                APS.availableProperties = APS.availableProperties :+ AvailableProperty(X.id, X.name, X.PropertyType,
-                  X.category, X.country, X.city, X.Price)
+                APS.availableProperties = APS.availableProperties :+ AvailableProperty(customerName, X.id, X.name,
+                  X.PropertyType, X.category, X.country, X.city, X.Price)
               }
             }
           }
@@ -329,43 +331,43 @@ class SystemService extends Actor{
       }
       replyTo ! APS
     }
-    case SendPropertyNameCategory(date, category, name, replyTo) => {
+    case SendPropertyNameCategory(customerName, date, category, name, replyTo) => {
       var APS = AvailableProperties(List())
       PROPERTIES.foreach {  X =>
         if (!X.NotAvailable.contains(date)){
           if (X.category == category){
             if (X.name == name) {
-              APS.availableProperties = APS.availableProperties :+ AvailableProperty(X.id, X.name, X.PropertyType,
-                X.category, X.country, X.city, X.Price)
+              APS.availableProperties = APS.availableProperties :+ AvailableProperty(customerName, X.id, X.name,
+                X.PropertyType, X.category, X.country, X.city, X.Price)
             }
           }
         }
       }
       replyTo ! APS
     }
-    case SendPropertyTypeName(date, propertyType, name, replyTo) => {
+    case SendPropertyTypeName(customerName, date, propertyType, name, replyTo) => {
       var APS = AvailableProperties(List())
       PROPERTIES.foreach {  X =>
         if (!X.NotAvailable.contains(date)){
           if (X.PropertyType == propertyType){
             if (X.name == name) {
-              APS.availableProperties = APS.availableProperties :+ AvailableProperty(X.id, X.name, X.PropertyType,
-                X.category, X.country, X.city, X.Price)
+              APS.availableProperties = APS.availableProperties :+ AvailableProperty(customerName, X.id, X.name,
+                X.PropertyType, X.category, X.country, X.city, X.Price)
             }
           }
         }
       }
       replyTo ! APS
     }
-    case SendPropertyTypeCountryCity(date, propertyType, country, city, replyTo) => {
+    case SendPropertyTypeCountryCity(customerName, date, propertyType, country, city, replyTo) => {
       var APS = AvailableProperties(List())
       PROPERTIES.foreach {  X =>
         if (!X.NotAvailable.contains(date)){
           if (X.PropertyType == propertyType){
             if (X.country == country){
               if (X.city == city){
-                APS.availableProperties = APS.availableProperties :+ AvailableProperty(X.id, X.name, X.PropertyType,
-                  X.category, X.country, X.city, X.Price)
+                APS.availableProperties = APS.availableProperties :+ AvailableProperty(customerName, X.id, X.name,
+                  X.PropertyType, X.category, X.country, X.city, X.Price)
               }
             }
           }
@@ -373,7 +375,7 @@ class SystemService extends Actor{
       }
       replyTo ! APS
     }
-    case SendPropertyTypeCountryCityName(date, propertyType, country, city, name, replyTo) => {
+    case SendPropertyTypeCountryCityName(customerName, date, propertyType, country, city, name, replyTo) => {
       var APS = AvailableProperties(List())
       PROPERTIES.foreach {  X =>
         if (!X.NotAvailable.contains(date)){
@@ -381,8 +383,8 @@ class SystemService extends Actor{
             if (X.country == country){
               if (X.city == city){
                 if (X.name == name){
-                  APS.availableProperties = APS.availableProperties :+ AvailableProperty(X.id, X.name, X.PropertyType,
-                    X.category, X.country, X.city, X.Price)
+                  APS.availableProperties = APS.availableProperties :+ AvailableProperty(customerName, X.id, X.name,
+                    X.PropertyType, X.category, X.country, X.city, X.Price)
                 }
               }
             }
